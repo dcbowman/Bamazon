4a88e7f0fc8b76850354d40139c1f684c034b3f9
Danielle Bowman <dcbowman6@yahoo.com>
2016-06-16 22:18:17 -0400

    prompts working

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
+
 var connection=mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -13,5 +15,15 @@ connection.connect(function(err){
 
 connection.query('SELECT * FROM Products',function(err,res){
 	for(var i=0;i<res.length;i++){
-		console.log(res[i].ProductName+" | "+res[i].DepartmentName+" | "+res[i].Price+" | "+res[i].StockQuantity);}
+		console.log(res[i].ProductName+" | "+res[i].DepartmentName+" | "+res[i].Price+" | "+res[i].StockQuantity);} //will list all of the items available pretty
 	console.log("-----------------------------------");})
+
+inquirer.prompt([{
+        type: 'input',
+        message: 'What is the ID of the item you wish to purchase?',
+        name: 'itemId'
+       },{
+       	type:'input',
+       	message: 'How many would you like?',
+       	name: 'quantity'
+       }])
