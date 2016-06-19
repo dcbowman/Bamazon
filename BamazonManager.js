6db6ebf8e19a4c1260a979ec5775a53af67a8bed
Danielle Bowman <dcbowman6@yahoo.com>
2016-06-19 13:39:32 -0400

    switch functions working

--- BamazonManager.js
+++ BamazonManager.js
@@ -10,5 +10,49 @@ var connection=mysql.createConnection({
 })
 connection.connect(function(err){
     if(err) throw err;
-    console.log("connected");//checks connection
-    console.log();})
+   mgrAction();
+})
+
+function mgrAction(){
+	inquirer.prompt({
+	name: "action",
+	type: "rawlist",
+	message: "What's on the agenda for today?",
+	choices: ["View items for sale", "View low inventory", "Add to inventory", "Add new product"]
+}).then(function(answer){
+	if (answer.action == "View items for sale"){
+		viewItems();
+	}
+	if (answer.action == "View low inventory"){
+		viewInventory();
+	}
+	if (answer.action == "Add to inventory"){
+		addInventory();
+	}
+	if (answer.action == "Add new product"){
+		addProduct();
+	}
+  })//end of .then
+}//end of mgrAction
+
+function viewItems(){
+	console.log("view");
+
+	mgrAction();
+}
+function viewInventory(){
+	console.log("inv");
+
+	mgrAction();
+}
+function addInventory(){
+	console.log("add");
+
+	mgrAction();
+}
+function addProduct(){
+	console.log("product");
+
+	mgrAction();
+}
+
