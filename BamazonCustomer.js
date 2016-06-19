d2fcc6e4f234e5b12cd7818362af2788e19e89a6
Danielle Bowman <dcbowman6@yahoo.com>
2016-06-19 12:23:24 -0400

    query returns product information

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -18,6 +18,10 @@ connection.query('SELECT * FROM Products',function(err,res){
 		console.log(res[i].ProductName+" | "+res[i].DepartmentName+" | "+res[i].Price+" | "+res[i].StockQuantity);} //will list all of the items available pretty
 	console.log("-----------------------------------");})
 
+//runs the order function
+order();
+
+function order(){
 inquirer.prompt([{
         type: 'input',
         message: 'What is the ID of the item you wish to purchase?',
@@ -26,4 +30,35 @@ inquirer.prompt([{
        	type:'input',
        	message: 'How many would you like?',
        	name: 'quantity'
-       }])
+       }]).then(function (answers) {
+        //console.log(answers.itemID);
+        //console.log(answers.quantity);
+        connection.query('SELECT * FROM Products WHERE ProductName = ?', answers.itemId, function(err,res){
+          if(err) throw err;
+
+          console.log(res);//
+  
+       checks to see if the quantity is enough
+
+          if (quantity > res[0].StockQuantiy){
+            console.log("Sorry! Not enough to complete order! Select a different amount");
+
+            //restarts
+            order();
+          }else{
+            console.log("cool");
+          }
+
+
+
+        })//end of query
+
+        
+});//end of.then fucntion
+}//end of order function
+
+
+
+
+
+
